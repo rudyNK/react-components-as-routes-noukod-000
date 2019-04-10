cfcf460cdc7248aca7fbb6d8ad8045f4e95d0c85
Rudy Ravelin <rudy@noukod.com>
2019-04-10 16:48:24 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,38 +1,21 @@
-// ./src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './components/home';
+import Login from './components/login';
+import About from './components/about';
+import Navbar from './components/navbar';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
 
-const About = () => {
-  return (
-    <div>
-      <h1>This is my about component!</h1>
-    </div>
-  );
-};
 
-const Login = () => {
-  return (
-    <div>
-      <form>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-          <label htmlFor="username">Username</label>
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-          <label htmlFor="password">Password</label>
-        </div>
-        <input type="submit" value="Login" />
-      </form>
-    </div>
-  );
-};
+ReactDOM.render(
+  <Router>
+    <React.Fragment>
+      <Navbar />
+      <Route path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/login' component={Login} />
+    </React.Fragment>
+  </Router>,
+  document.getElementById('root')
+);
